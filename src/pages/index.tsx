93818b402bcb115410834c31ae97a1272b92f8e7
ygkn <2000ygkn0713@gmail.com>
2021-02-15 23:08:05 +0900

    edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
-import { VFC } from 'react';
+import React, { useMemo, useState, VFC } from 'react';
+import { createEditor, Node } from 'slate';
+import { Editable, Slate, withReact } from 'slate-react';
 
 const IndexPage: VFC = () => {
-  return null;
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]);
+
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(newValue) => setValue(newValue)}
+    >
+      <Editable />
+    </Slate>
+  );
 };
 
 export default IndexPage;
